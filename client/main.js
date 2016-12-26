f98be0e8aa8ca6e0b196558632e5b240264d12d0
Derek Gransaull <derek@dgtlife.com>
2016-12-26 02:43:05 -0800

    Updated the API docs to reflect new export approach.

--- client/main.js
+++ client/main.js
@@ -5,3 +5,7 @@
  * Created on 11/2/16
  */
 import '../imports/startup/client/index.js';
+
+// ToDo: Remove all CoffeeScript?
+// ToDo: Create links and screens to demo different header panel modes.
+// ToDo: Fix the Tab initialization issue.
